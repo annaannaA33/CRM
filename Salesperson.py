bc1ea63cb193c79b1f8710f071f37e2cfe3854a6
annaannaA33 <129603016+annaannaA33@users.noreply.github.com>
2024-02-10 15:26:21 +0200

    Update Salesperson.py

--- Salesperson.py
+++ Salesperson.py
@@ -1,13 +1,23 @@
-class Salesperson:
-    def __init__(self, name):
-        self.name = name
+from Ticket import Ticket
 
-    def view_sales_requests(self):
-        # Implement logic to view sales requests
-        pass
+class Salesperson(Ticket):
+    def __init__(self):
+        self.tickets = []  # List to hold tickets assigned to the salesperson
 
-    def respond_to_request(self, request, response, new_operator):
-        request.description += f"\nResponse: {response}"
-        request.executor = new_operator
-        # Implement logic to save changes to the request
-        pass
+
+
+    def find_ticket(self, ticket_number, tickets):
+        # Вызов метода find_ticket родительского класса Ticket
+        ticket = super().find_ticket(ticket_number, tickets)
+        if ticket and ticket.executor == "SALES_DEPT":
+            return ticket
+        else:
+            return None
+        
+    def process_the_lead(ticket):
+        print("Обработайте заявку, после внесите решение по заявке. После внесения решения заявка вернется к оператору на проверку.")
+        
+        ticket._solution = input("Введите решение: ")
+        return ticket._solution
+
+
